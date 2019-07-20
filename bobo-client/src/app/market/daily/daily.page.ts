212ae2f00fac7d1aff4e4101ee3eedb0d6d66841
Min Joo Kang <mjk10@kaist.ac.kr>
2019-07-21 04:34:28 +0900

    add daily stuffz

--- bobo-client/src/app/market/daily/daily.page.ts
+++ bobo-client/src/app/market/daily/daily.page.ts
@@ -1,12 +1,55 @@
 import {Component, OnInit} from '@angular/core';
+import {NavController} from '@ionic/angular';
+import {Product} from './Product';
 
 @Component({
-    selector: 'app-market',
-    templateUrl: 'market.page.html',
-    styleUrls: ['market.page.scss']
+    selector: 'app-market-daily',
+    templateUrl: 'daily.page.html',
+    styleUrls: ['daily.page.scss']
 })
-export class ProductsPage implements OnInit {
-    constructor() {
+export class ProductListPage implements OnInit {
+
+    public products: Product[] = [];
+    constructor(public navCtrl: NavController) {
+        let products = [
+            new Product(
+                'Water 10L',
+                18,
+                '../assets/products/images/water.jpeg',
+            ),
+            new Product(
+                'Basic White Shirt x 50',
+                200,
+                '../assets/products/images/shirt.jpeg',
+            ),
+            new Product(
+                'Instant Rice x 50',
+                100,
+                '../assets/products/images/rice.jpeg',
+            ),
+            new Product(
+                'Instant Cup Noodles x 50',
+                100,
+                '../assets/products/images/noodles.jpeg',
+            ),
+        ];
+        // Increase array items to apply page scroll
+        products = products.concat(products).concat(products).concat(products)
+
+        // Re-create products so it will not have the same memory address
+        this.products = products.map(prod => new Product(
+            prod.name,
+            prod.price,
+            prod.img,
+        ));
+    }
+
+    get finalValue(): number {
+        return this.products.reduce((sum, prod) => sum + (prod._quantity * prod.price), 0);
+    }
+
+    Purchase() {
+        console.log(`Value: ${this.finalValue} Dai`);
     }
 
     ngOnInit() {
