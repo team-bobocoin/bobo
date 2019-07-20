1b6d01103435d64be64148865196178d3f9f292a
차민준 <minjun@chaminjun-ui-MacBookPro.local>
2019-07-20 13:11:08 +0900

    Add Sponsor list page
    Add Sponsor list page. Sponser list page is that show the list of donation.

--- bobo-client/src/app/app-routing.module.ts
+++ bobo-client/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
     {path: '', loadChildren: './tabs/tabs.module#TabsPageModule'},
     {path: 'helpee', loadChildren: './tab1/helpee/helpee.module#HelpeeModule'},
     {path: 'donate', loadChildren: './tab1/helpee/donate/donate.module#DonateModule'},
+    {path: 'sponsor-list', loadChildren: './tab2/sponsor-list/sponsor_list.module#SponsorListModule'}
 ];
 
 @NgModule({
